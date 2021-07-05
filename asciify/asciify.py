9495c1d1ba7d9f4743323c3373cd84a0fc32a594
Shiva Tamil Kumaran <anand.tk.03@gmail.com>
2021-07-05 13:07:54 +0000

    Fixed argparser

--- asciify/asciify.py
+++ asciify/asciify.py
@@ -4,32 +4,20 @@ from PIL import Image
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser 
+    parser = argparse.ArgumentParser() 
     parser.add_argument("-i", "--image", type=str, help="The filepath to the image you want to asciify")
     parser.add_argument("-w", "--width", type=int, help="The number of characters wide the output text will be. Defaults to 50.", default=50)
-    parser.add_argument("-I", "--inverted", type=bool, help="A boolean representing whether or not the output image should invert black and white. Choose False for black text on white background.", default=False)
-    
-    argv = sys.argv
-    try:
-        image = Image.open(argv[1])
-    except:
-        print("Error: Image not found.")
-        print("Please refer to the readme file for more information.")
-        sys.exit()
+    parser.add_argument("-I", "--inverted", type=bool, help="A boolean representing whether or not the output image should invert black and white. Choose False for black text on white background. Defaults to false.", default=False)
+
+    args = parser.parse_args()
     
     try:
-        charWidth = int(argv[2])
+        image = Image.open(args.image)
     except:
-        print("Error: Invalid width.")
-        print("Please refer to the readme file for more information.")
+        print("usage: asciify [-h] [-i IMAGE] [-w WIDTH] [-I INVERTED]")
+        print("asciify: error: image not found")
         sys.exit()
     
-    try:
-        invert = bool(argv[3])
-    except:
-        print("Error: The \'Inverted\' arguement is not specified correctly.")
-        print("Please refer to the readme file for more information.")
-        sys.exit()
     
-    print(asciify(charWidth, image, invert))
+    print(asciify(args.width, image, args.inverted))
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "0.1.2"
+version = "0.1.3"
 
 with open("README.md", "r") as f:
     long_descr = f.read()
